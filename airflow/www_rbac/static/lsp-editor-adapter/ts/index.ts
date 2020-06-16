844aab24a90f39a1efe319a5b0e9a157f82cfab6
Jai Doshi <jai@couture.ai>
2020-06-16 16:11:22 +0530

    Added CodeMirror adapter

---
+++ airflow/www_rbac/static/lsp-editor-adapter/ts/index.ts
@@ -0,0 +1,65 @@
+import * as CodeMirror from 'codemirror';
+import 'codemirror/mode/python/python';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/idea.css';
+import 'codemirror/addon/fold/foldgutter.css';
+import 'codemirror/addon/dialog/dialog.css';
+import 'codemirror/addon/search/searchcursor'
+import 'codemirror/addon/search/search'
+import 'codemirror/addon/dialog/dialog'
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/wrap/hardwrap';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/indent-fold';
+import 'codemirror/addon/selection/active-line';
+import 'codemirror/keymap/sublime';
+
+
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror/addon/hint/show-hint';
+
+import '../lib/codemirror-lsp.css';
+import { LspWsConnection, CodeMirrorAdapter } from '../lib/index';
+
+
+const theme = 'idea', keymap = 'sublime';
+var Configs = {
+    lineNumbers: true,
+    smartIndent: true,
+    mode: 'python',
+    styleActiveLine: true,
+    autofocus: true,
+    theme: theme,
+    foldGutter: true,
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lsp"],
+    keyMap: keymap
+  };
+
+var myTextArea = document.getElementById('code') as HTMLTextAreaElement;
+var codeEditor = CodeMirror.fromTextArea(myTextArea, Configs);
+
+function connectToLangServer (dags_folder_path: string, filename: string): void
+{
+    let py  = {
+        serverUri: 'ws://localhost:3000/python',
+        languageId: 'python',
+        rootUri: 'file://' + dags_folder_path,
+        documentUri: 'file://' + dags_folder_path + '/' + filename,
+        documentText: () => codeEditor.getValue()
+        };
+
+
+    let pyConnection = new LspWsConnection(py).connect(new WebSocket(py.serverUri));
+    let pyAdapter = new CodeMirrorAdapter(pyConnection, {
+      quickSuggestionsDelay: 50,
+      }, codeEditor);
+
+}   
+
+export {codeEditor};
+export {connectToLangServer};
+    
---
+++ airflow/www_rbac/static/lsp-editor-adapter/ts/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: './index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  target: 'web',
+  node: {
+    net: 'mock',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'langServer',
+    libraryTarget: 'var'
+  }
+};
