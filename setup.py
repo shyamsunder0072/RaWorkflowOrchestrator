0235733887c5ec8fd33e841bf25ec138842a0d90
shyamsunder0072 <50831648+shyamsunder0072@users.noreply.github.com>
2021-03-23 02:34:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ celery = [
     'celery~=4.3',
     'flower>=0.7.3, <1.0',
     'kombu==4.6.3;python_version<"3.0"',
-    'tornado>=4.2.0, <6.0',  # Dep of flower. Pin to a version that works on Py3.5.2
+    'tornado>=4.2.0, <7.0',  # Dep of flower. Pin to a version that works on Py3.5.2
 ]
 cgroups = [
     'cgroupspy>=0.1.4',
